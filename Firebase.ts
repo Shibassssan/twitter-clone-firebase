23a458c0d80b8ca3e687c94a76eba60e2031ad89
Shibassssan <shibatayuichiworks@gmail.com>
2023-02-23 19:20:05 +0900

    [add] Firebaseの初期化の設定記述 追加

---
+++ Firebase.ts
@@ -0,0 +1,28 @@
+import { initializeApp, getApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFunctions } from "firebase/functions";
+import "dotenv/config";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+const auth = getAuth(app);
+const storage = getStorage(app);
+const analytics = getAnalytics(app);
+const functions = getFunctions(app);
+const provider = new GoogleAuthProvider();
+
+export { db, auth, storage, analytics, functions, provider };
