cdf3816b47b627b3067e558aa7b8054d5855c777
Shibassssan <shibatayuichiworks@gmail.com>
2023-03-11 13:15:35 +0900

    [add] validationの正規表現とエラーメッセージのまとめを追加

---
+++ src/define.ts
@@ -0,0 +1,21 @@
+export const ERROR_MESSAGE = {
+  REQUIRE: '必須項目です。',
+  EMAIL_FORMAT_ERROR: 'メールアドレスの形式で入力してください。',
+  INVALID_CHARACTER: '全て半角英数字、半角記号で入力してください。',
+  MINIMUM_LENGTH: '8文字以上入力してください。',
+  ALPHABET_REQUIRED: '英字を1文字以上含めてください。',
+  DIGIT_REQUIRED: '数字を1文字以上含めてください。',
+  CASE_REQUIRED: '大文字小文字をそれぞれ1文字以上含めてください。',
+  SYMBOL_REQUIRED: '記号を1文字以上含めてください。'
+} as const;
+
+
+export const Regex = {
+  emailFormat: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+  containsAlphanumeric: /^[a-zA-Z0-9!-/:-@¥[-`{-~]+$/,
+  containsAlphabet: /[a-zA-Z]+/,
+  containsNumber: /\d+/,
+  containsLowerUpper: /(?=.*[a-z])(?=.*[A-Z])/,
+  containsSymbol: /[-!"#$%&'()*+,./:;<=>?@[\\\]^_`{|}~]+/,
+};
+
