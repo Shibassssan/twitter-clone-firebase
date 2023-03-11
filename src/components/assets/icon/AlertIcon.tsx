b5ea49d4dadae5a68d8d64242edbbe550353c43f
Shibassssan <shibatayuichiworks@gmail.com>
2023-03-11 17:41:22 +0900

    [add] iconを追加

---
+++ src/components/assets/icon/AlertIcon.tsx
@@ -0,0 +1,12 @@
+export const AlertIcon = () => (
+  <svg
+    version="1.1"
+    xmlns="http://www.w3.org/2000/svg"
+    width="30px"
+    height="20px"
+    viewBox="0 0 28 28"
+  >
+    <title>iconCrossBlack</title>
+    <path d="M28 4.375l-9.625 9.625 9.625 9.625-4.375 4.375-9.625-9.625-9.625 9.625-4.375-4.375 9.625-9.625-9.625-9.625 4.375-4.375 9.625 9.625 9.625-9.625 4.375 4.375z"></path>
+  </svg>
+);
---
+++ src/components/assets/icon/CheckIcon.tsx
@@ -0,0 +1,12 @@
+export const CheckIcon = () => (
+  <svg
+    version="1.1"
+    xmlns="http://www.w3.org/2000/svg"
+    width="30px"
+    height="20px"
+    viewBox="0 0 28 28"
+  >
+    <title>iconCheck</title>
+    <path d="M28.027 5.697l-18.834 18.834-9.193-9.193 2.635-2.635 6.558 6.586 16.2-16.2 2.635 2.607z"></path>
+  </svg>
+);
