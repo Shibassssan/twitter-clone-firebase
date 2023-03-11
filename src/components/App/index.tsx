b9bb3ae3a300b5ba90e8064d033999fff4cceb20
Shibassssan <shibatayuichiworks@gmail.com>
2023-03-11 11:13:21 +0900

    [feature] App.cssファイルを削除

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -10,7 +10,8 @@ import {
 import { style } from './style';
 import { Footer } from '../Footer';
 import { Header } from '../Header';
-import  { Top } from '../Top'
+import { Top } from '../Top';
+import { Login } from '../Login';
 
 type LoginContextType = {
   isLogin: boolean;
--- src/components/App/style.tsx
+++ src/components/App/style.tsx
@@ -5,9 +5,22 @@ export const style = css`
     text-align: center;
   }
 
+  header {
+    height: 48px;
+    display: flex;
+    z-index: 3;
+    box-shadow: 0 4px 2px -2px rgb(0 0 0 / 20%);
+    align-items: center;
+  }
+
   main {
     flex: auto;
     margin: 0 0 50px;
+  }
+
+  footer {
+    margin-top: 20px;
+    background: grey;
   }
 `;
 
