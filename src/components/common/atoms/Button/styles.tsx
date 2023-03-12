3ea1fd5e609c52cd5729ee32452b187b6fd26b98
Shibassssan <shibatayuichiworks@gmail.com>
2023-03-12 15:17:41 +0900

    [feature] ButtonとInputをatoms配下に移行

--- src/components/common/atoms/Button/styles.tsx
+++ src/components/common/atoms/Button/styles.tsx
@@ -1,5 +1,5 @@
 import { css } from '@emotion/react';
-import { Color } from '../../../Style';
+import { Color } from '../../../../Style';
 
 export type BackgroundColorType = 'primary' | 'secondary' | 'other';
 
