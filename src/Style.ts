878777166707c0ef79cd3f7e456d80dd7538c50d
Shibassssan <shibatayuichiworks@gmail.com>
2023-03-12 12:57:17 +0900

    [feature] サイトカラーを変更 & ボーターカラーを追加

--- src/Style.ts
+++ src/Style.ts
@@ -1,9 +1,10 @@
 import { css } from '@emotion/react';
 
 export const Color = {
-  Primary: '#0051e0',
+  Primary: 'rgb(29, 155, 240)',
   White: '#fff',
   Black: '#000',
   Error: '#f9423a',
   Check: '#009736',
+  Border: 'rgb(239, 243, 244)',
 };
