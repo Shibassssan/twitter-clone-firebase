e96b5b508f240b57e2292965efc79b55e0495fe4
Shibassssan <shibatayuichiworks@gmail.com>
2023-03-12 16:04:53 +0900

    [feature] font weiohtの定義追加

--- src/Style.ts
+++ src/Style.ts
@@ -1,10 +1,18 @@
-import { css } from '@emotion/react';
-
 export const Color = {
   Primary: 'rgb(29, 155, 240)',
   White: '#fff',
+  PreWhite: 'rgb(83, 100, 113)',
   Black: '#000',
   Error: '#f9423a',
   Check: '#009736',
   Border: 'rgb(239, 243, 244)',
 };
+
+export const Font = {
+  Size: {
+    Default: 14,
+  },
+  Weight: {
+    Bold: 700
+  }
+}
