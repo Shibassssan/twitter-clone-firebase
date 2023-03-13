6893f6425d93a23129a744905eb452694524a065
Shibassssan <shibatayuichiworks@gmail.com>
2023-03-13 09:22:56 +0900

    [create] 複数ページ作成

---
+++ src/pages/explore.tsx
@@ -0,0 +1,19 @@
+/** @jsxImportSource @emotion/react */
+import { FC } from 'react';
+import { Footer } from '../components/Footer';
+import { Header } from '../components/Header';
+
+/** キーワード検索 */
+const Explore: FC = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <div></div>
+      </main>
+      <Footer />
+    </>
+  )
+};
+
+export default Explore;
---
+++ src/pages/messages.tsx
@@ -0,0 +1,19 @@
+/** @jsxImportSource @emotion/react */
+import { FC } from 'react';
+import { Footer } from '../components/Footer';
+import { Header } from '../components/Header';
+
+/** DM ページ */
+const Messages: FC = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <div></div>
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default Messages;
