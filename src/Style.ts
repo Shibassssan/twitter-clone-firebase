390ae4886c6e6b77c4eb94791ab6155998a6641f
Shibassssan <shibatayuichiworks@gmail.com>
2023-03-15 00:54:42 +0900

    [feature] media queryの定義追加 & Color 2色追加

--- src/Style.ts
+++ src/Style.ts
@@ -1,8 +1,10 @@
 export const Color = {
   Primary: 'rgb(29, 155, 240)',
   White: '#fff',
+  White2: '#536471',
   PreWhite: 'rgb(83, 100, 113)',
   Black: '#000',
+  Black2: 'rgb(15, 20, 25)',
   Error: '#f9423a',
   Check: '#009736',
   Border: 'rgb(239, 243, 244)',
@@ -13,6 +15,17 @@ export const Font = {
     Default: 14,
   },
   Weight: {
-    Bold: 700
+    Bold: 700,
   }
+}
+
+const BreakPoints = {
+  Mobile: 365,
+  Tablet: 960,
+}
+
+export const MQ = {
+  Mobile: `@media (max-width: ${BreakPoints.Mobile})`,
+  Tablet: `@media (max-width: ${BreakPoints.Tablet}, min-width: ${BreakPoints.Mobile + 1})`,
+  Desktop: `@media (min-width: ${BreakPoints.Tablet + 1})`
 }
