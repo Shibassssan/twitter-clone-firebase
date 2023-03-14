0db6e073b24d8facc2a7fdd0a203a4e0891cfd2f
Shibassssan <shibatayuichiworks@gmail.com>
2023-03-15 00:56:38 +0900

    [add] sideMenuで使用するicon追加

---
+++ src/components/assets/icon/Accordion.tsx
@@ -0,0 +1,10 @@
+export const AccordionIcon = () => (
+  <svg
+    viewBox="0 0 24 24"
+    aria-hidden="true"
+  >
+    <g>
+      <path d="M3.543 8.96l1.414-1.42L12 14.59l7.043-7.05 1.414 1.42L12 17.41 3.543 8.96z"></path>
+    </g>
+  </svg>
+);
