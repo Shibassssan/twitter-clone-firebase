51f4a4dae2644973152ffd52e700976141b27acb
Shibassssan <shibatayuichiworks@gmail.com>
2023-03-26 23:01:57 +0900

    [remove] 不要になった処理を削除

--- src/components/containers/Top/index.tsx
+++ src/components/containers/Top/index.tsx
@@ -1,4 +1,3 @@
-/** @jsxImportSource @emotion/react */
 import {
   FC,
   useCallback,
@@ -7,21 +6,15 @@ import {
   useContext,
 } from 'react';
 import {
-  addDoc,
-  getDocs,
   collection,
-  serverTimestamp,
   FieldValue,
   doc,
   onSnapshot,
 } from 'firebase/firestore';
 import app, { db } from '~/src/Firebase';
 import {
-  signOut,
   getAuth
 } from 'firebase/auth';
-import Cookies from 'js-cookie';
-import { COOKIES } from '~/src/define';
 import { TopStyle } from './style';
 import { UserContext } from '~/src/pages/_app';
 import { TweetCard } from '~/src/components/common/organisms/TweetCard';
@@ -35,28 +28,8 @@ type Tweets = {
 const auth = getAuth(app);
 
 export const Top: FC = () => {
-  const { userInfo, setUserInfo } = useContext(UserContext);
+  const { userInfo } = useContext(UserContext);
 
-  const [text, setText] = useState('');
-  const onChangeText = useCallback(
-    (e: React.ChangeEvent<HTMLTextAreaElement>) => setText(e.target.value),
-    []
-  );
-  const sendTweet = useCallback(async () => {
-    if (!text) return;
-    try {
-      const postCollection = collection(db, 'posts');
-      const result = await addDoc(postCollection, {
-        // id: postCollection.id,
-        userName: 'hoge3',
-        tweet: text,
-        timestamp: serverTimestamp(),
-      });
-      console.log('result', result);
-    } catch (error) {
-      console.log('error!!!!!', error);
-    }
-  }, [text]);
 
   const [tweets, setTweets] = useState<Tweets[]>([]);
   const getTweets = useCallback(async () => {
@@ -71,34 +44,7 @@ export const Top: FC = () => {
     }
   }, []);
 
-  const logout = async () => {
-    try {
-      await signOut(auth);
-      setUserInfo({
-        name: '',
-        photoUrl: '',
-        isLogin: false,
-        userId: '',
-      });
-      Cookies.remove(COOKIES.MEMBER)
-    } catch (error) {
-      console.error('error', error);
-    }
-  };
-
-  const getUserInfo = useCallback(() => {
-    const user = auth.currentUser;
-    if (!user) return;
-    setUserInfo({
-      userId: user.uid,
-      name: user.displayName || '',
-      photoUrl: user.photoURL || '',
-      isLogin: true,
-    })
-  }, []);
-
   useEffect(() => {
-    getUserInfo();
     getTweets();
   }, []);
 
