1c2b8a557d37fd8bf57ba02c4a39b2e700be060b
Shibassssan <shibatayuichiworks@gmail.com>
2023-03-26 16:09:23 +0900

    [feature] ツイートcomponent完成

---
+++ src/components/containers/Tweet/index.tsx
@@ -0,0 +1,141 @@
+import {
+  FC,
+  useCallback,
+  useContext,
+  useState,
+} from 'react';
+import {
+  addDoc,
+  getDocs,
+  collection,
+  serverTimestamp,
+  FieldValue,
+  doc,
+  onSnapshot,
+} from 'firebase/firestore';
+import { style, TweetStyle } from './styles';
+import { ArrowIcon } from '~/src/assets/icon/Arrow';
+import { Button } from '~/src/components/common/atoms/Button';
+import { useRouter } from 'next/router';
+import { UserContext } from '~/src/pages/_app';
+
+// icon
+import { SmileIcon } from '~/src/assets/icon/Smile';
+import { GifIcon } from '~/src/assets/icon/Gif';
+import { LocationIcon } from '~/src/assets/icon/Location';
+import { EarthIcon } from '~/src/assets/icon/Earth';
+import { ImageIcon } from '~/src/assets/icon/Image';
+import { VoteIcon } from '~/src/assets/icon/Vote';
+import { ScheduleIcon } from '~/src/assets/icon/Schedule';
+import Image from 'next/image';
+import avator from '~/src/assets/images/avator.png';
+import { AccordionIcon } from '~/src/assets/icon/Accordion';
+
+
+export const TweetContainer: FC = () => {
+  const router = useRouter();
+  const { userInfo, setUserInfo } = useContext(UserContext);
+
+  const [text, setText] = useState('');
+  const onChangeText = useCallback(
+    (e: React.ChangeEvent<HTMLTextAreaElement>) => setText(e.target.value),
+    []
+  );
+
+  const sendTweet = useCallback(async () => {
+    if (!text) return;
+    try {
+      const postCollection = collection(db, 'posts');
+      const result = await addDoc(postCollection, {
+        // id: postCollection.id,
+        userName: 'hoge3',
+        tweet: text,
+        timestamp: serverTimestamp(),
+      });
+      console.log('result', result);
+    } catch (error) {
+      console.log('error!!!!!', error);
+    }
+  }, [text]);
+
+  return (
+    <div css={style}>
+      <section className="header">
+        <div className="backButton" onClick={() => router.back()}>
+          <ArrowIcon />
+        </div>
+        <div></div>
+        <div className="tweetButton">
+          <Button label={'ツイートする'} type="primary" />
+        </div>
+      </section>
+      <section className="body">
+        <div css={TweetStyle}>
+          <div className="userImage">
+            <Image
+              src={userInfo.photoUrl || avator}
+              width={46}
+              height={46}
+              alt="ユーザーアイコン"
+            />
+          </div>
+          <div className="tweetWrap">
+            <div className="selectPublic">
+              <div className='labelWrap'>
+                <div className='label'>
+                  <span>全員</span>
+                  <AccordionIcon />
+                </div>
+              </div>
+            </div>
+            <div className='text'>
+              <textarea
+                name="ツイート本文"
+                placeholder='いまどうしてる？'
+                aria-label='テキストをツイート'
+                autoComplete='on'
+                autoCorrect='on'
+                spellCheck
+                dir='auto'
+                onChange={(e) => onChangeText(e)}
+              />
+            </div>
+            <div className='commentTarget'>
+              <EarthIcon />
+              <div>全員に返信できます</div>
+            </div>
+            <div className='toolBar'>
+              <ul>
+                <li>
+                  <div>
+                    <ImageIcon />
+                  </div>
+                </li>
+                <li>
+                  <div>
+                    <GifIcon />
+                  </div>
+                </li>
+                <li>
+                  <div>
+                    <VoteIcon />
+                  </div>
+                </li>
+                <li>
+                  <div>
+                    <ScheduleIcon />
+                  </div>
+                </li>
+                <li>
+                  <div>
+                    <LocationIcon />
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
---
+++ src/components/containers/Tweet/styles.tsx
@@ -0,0 +1,120 @@
+import { css } from '@emotion/react';
+import { Color, Font } from '~/src/Style';
+
+export const style = css`
+  display: flex;
+  flex-direction: column;
+
+  & .header {
+    height: 50px;
+    padding: 0 15px;
+    display: grid;
+    grid-template-columns: 1fr 3fr 2fr;
+
+    & .backButton {
+      display: flex;
+      align-items: center;
+      min-height: 34px;
+      min-width: 34px;
+    }
+
+    & .tweetButton {
+      display: flex;
+      align-items: center;
+      width: 100%;
+    }
+
+    svg {
+      width: 24px;
+      height: 24px;
+    }
+  }
+
+  & .body {
+    padding: 0 15px;
+  }
+`;
+
+export const TweetStyle = css`
+  display: flex;
+
+  & .userImage {
+    margin-right: 10px;
+    padding-top: 4px;
+    width: auto;
+  }
+
+  & .tweetWrap {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    padding-top: 5px;
+
+    & .selectPublic {
+      display: flex;
+      padding-bottom: 10px;
+
+      & .labelWrap {
+        color: ${Color.Primary};
+        font-size: ${Font.Size.Default}px;
+
+        & .label {
+          display: flex;
+          align-items: center;
+          padding: 0 11px;
+          border: 1px solid ${Color.Border2};
+          border-radius: 99999px;
+        }
+      }
+    }
+
+    & .text {
+      padding: 10px 0;
+
+      > textarea {
+        outline: none;
+        appearance: none;
+        border: none;
+        resize: none;
+        min-height: 114px;
+        width: 100%;
+        font-size: ${Font.Size.Big1}px;
+      }
+    }
+
+    svg {
+      width: 15px;
+      height: 15px;
+      fill: ${Color.Primary};
+    }
+  }
+
+  & .commentTarget {
+    display: grid;
+    grid-template-columns: 21px auto;
+    align-items: center;
+    padding-bottom: 11px;
+    border-bottom: 1px solid ${Color.Border};
+    color: ${Color.Primary};
+  }
+
+  & .toolBar {
+    display: flex;
+    margin-top: 10px;
+    > ul {
+      display: inherit;
+
+      > li {
+        display: grid;
+        min-width: 34px;
+        min-height: 34px;
+        place-content: center;
+      }
+    }
+
+    svg {
+      width: 19px;
+      height: 19px;
+    }
+  }
+`;
