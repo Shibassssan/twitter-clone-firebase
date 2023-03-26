c1bf1038c460e8354c5538a1a9690d384dc5adb7
Shibassssan <shibatayuichiworks@gmail.com>
2023-03-26 16:09:58 +0900

    [feature] ツイートcomponent用の導線Pageを追加

---
+++ src/pages/compose/tweet.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next";
+import { TweetContainer } from "~/src/components/containers/Tweet";
+
+const Tweet:NextPage = () => {
+  return (
+    <>
+      <TweetContainer />
+    </>
+  )
+}
+
+export default Tweet;
