0d772bb667d7043566185051865c3c739d5b1126
Shibassssan <shibatayuichiworks@gmail.com>
2023-05-27 17:23:35 +0900

    [feature] Footer icon Activeときりかえ

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,15 +1,28 @@
 /** @jsxImportSource @emotion/react */
-import { FC, useCallback, useState } from 'react';
+import { FC } from 'react';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { footerStyle, linkButtonStickyStyle } from './style';
 import { Home as HomeIcon } from '~/src/assets/icon/Home';
+import { HomeActive as HomeActiveIcon } from '~/src/assets/icon/HomeActive';
 import { Search as SearchIcon } from '~/src/assets/icon/Search';
+import { SearchActive as SearchActiveIcon } from '~/src/assets/icon/SearchActive';
 import { Notice as NoticeIcon } from '~/src/assets/icon/Notice';
+import { NoticeActive as NoticeActiveIcon } from '~/src/assets/icon/NoticeActive';
 import { Mail as MailIcon } from '~/src/assets/icon/Mail';
+import { MessageActive as MessageActiveIcon } from '~/src/assets/icon/MessageActive';
 import { CircleButton } from '../common/molecules/CircleButton';
+
+const Page = {
+  Top: '/',
+  Search: '/explore',
+  Notice: '/notifications',
+  Message: '/messages'
+} as const;
 
 
 export const Footer: FC = () => {
+  const router = useRouter();
   return (
     <>
       <div className="linkButtonSticky" css={linkButtonStickyStyle}>
@@ -19,23 +32,35 @@ export const Footer: FC = () => {
         <nav>
           {/* TODO リンクに置き換える */}
           <div>
-            <Link href={'/'}>
-              <HomeIcon />
+            <Link href={Page.Top}>
+              {router.pathname === Page.Top ? <HomeActiveIcon /> : <HomeIcon />}
             </Link>
           </div>
           <div>
-            <Link href={'/explore'}>
-              <SearchIcon />
+            <Link href={Page.Search}>
+              {router.pathname === Page.Search ? (
+                <SearchActiveIcon />
+              ) : (
+                <SearchIcon />
+              )}
             </Link>
           </div>
           <div>
-            <Link href={'/notifications'}>
-              <NoticeIcon />
+            <Link href={Page.Notice}>
+              {router.pathname === Page.Notice ? (
+                <NoticeActiveIcon />
+              ) : (
+                <NoticeIcon />
+              )}
             </Link>
           </div>
           <div>
-            <Link href={'/messages'}>
-              <MailIcon />
+            <Link href={Page.Message}>
+              {router.pathname === Page.Message ? (
+                <MessageActiveIcon />
+              ) : (
+                <MailIcon />
+              )}
             </Link>
           </div>
         </nav>
